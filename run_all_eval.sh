33d90db3062dd9e43158e609e7a5d86ee6647071
Sha Zhizhou (zs7752) <zhizhousha@utexas.edu>
2025-11-18 16:55:36 -0600

    update

--- run_all_eval.sh
+++ run_all_eval.sh
@@ -5,8 +5,8 @@ set -e
 # this will lead to OOM
 
 # base_dir="DeepScaleR-1.5B-Preview_sigma-blend_fp32"
-
-base_dir="Nemotron-Research-Reasoning-Qwen-1.5B_sigma-blend_fp32"
+# base_dir="Nemotron-Research-Reasoning-Qwen-1.5B_sigma-blend_fp32"
+base_dir="DeepSeek-R1-Distill-Qwen-1.5B_sigma-blend_fp32"
 
 alphas=(0.0 0.2 0.4 0.6 0.8 1.0)
 # alphas=(0.8 1.0)
@@ -15,8 +15,12 @@ gpus=(0 1 2 3)
 num_gpus=${#gpus[@]}
 max_tokens=$((16*1024))
 n_samples=16
-tokenizer="nvidia/Nemotron-Research-Reasoning-Qwen-1.5B"  # 用原始模型的 tokenizer
-# tokenizer_basename=$(basename $tokenizer)
+
+tokenizer="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B" 
+
+# tokenizer="nvidia/Nemotron-Research-Reasoning-Qwen-1.5B"  # 用原始模型的 tokenizer
+
+tokenizer_basename=$(basename $tokenizer)
 
 dataset_list=("HuggingFaceH4/aime_2024" "math-ai/amc23" "HuggingFaceH4/math-500")
 
@@ -28,7 +32,7 @@ for i in "${!alphas[@]}"; do
     alpha=${alphas[$i]}
     gpu=${gpus[$((i % num_gpus))]}
     model_path="${base_dir}/alpha_${alpha}"
-    model_name="alpha_${alpha}_$(basename $data_path)"
+    model_name="${tokenizer_basename}_alpha_${alpha}_$(basename $data_path)"
 
         (
             export CUDA_VISIBLE_DEVICES=$gpu
